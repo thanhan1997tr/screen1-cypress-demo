ba96351a6bed4be325c538a971b3b5741ac294e7
thanhan1997tr <thanhan1997tr@gmail.com>
2025-01-08 15:02:09 +0700

    Enable test isolation in Cypress configuration

--- cypress.config.ts
+++ cypress.config.ts
@@ -9,6 +9,6 @@ export default defineConfig({
             // implement node event listeners here
         },
         // baseUrl: process.env.BASE_URL,
-        // testIsolation: false,
+        testIsolation: false,
     },
 });
